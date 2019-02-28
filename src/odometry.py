6969a97914c0866495acf081bb8534b32deacd11
Peggy <peggy.hoelzel@outlook.de>
2019-02-28 13:27:22 +0100

    added simple test

--- src/odometry.py
+++ src/odometry.py
@@ -5,33 +5,64 @@
 
 import ev3dev.ev3 as ev3
 
-class LineFollower():
+
+class LineFollower:
     def __init__(self):
         stop = False
 
     def drive(self):
-        stop = False
+        stop = True
 
-        #sensors
+        # sensors
         colorSensor = ev3.ColorSensor()
+        lightSensor = ev3.LightSensor()
         ultrasonicSensor = ev3.UltrasonicSensor()
 
         assert colorSensor.connected
+        assert lightSensor.connected
         assert ultrasonicSensor.connected
 
-        #motors
+        colorSensor.mode = 'COL-COLOR'
+        lightSensor.mode = 'REFLECT'
+
+        # motors
         rightMotor = ev3.LargeMotor(ev3.OUTPUT_C)
         leftMotor = ev3.LargeMotor(ev3.OUTPUT_B)
 
         assert rightMotor.connected
         assert leftMotor.connected
 
-        while(not stop):
-            #drive
+        kp = 1000   # kp*100 -> 10
+        ki = 100    # ki*100 -> 1
+        kd = 10000  # kd*100 -> 100
+        offset = 45
+        tp = 50
+        integral = 0
+        lastError = 0
+        derivative = 0
+
+        while not stop:
+            lightValue = lightSensor.value()
+            error = lightValue - offset
+            integral += error
+            derivative = error - lastError
+            turn = (kp * error) + (ki * integral) + (kd * derivative)
+            turn /= 100
+            powerLeft = tp + turn
+            powerRight = tp - turn
+
+
+
+            lastError = error
+
+        rightMotor.run_timed(time_sp = 600)
+        leftMotor.run_timed(time_sp = 600)
 
 
 class Odometry():
-    #code
+    x=42
+
 
 if __name__ == "__main__":
-    #run LineFollower
+    robot = LineFollower()
+    robot.drive()
