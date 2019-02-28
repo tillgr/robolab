daa95734b4f598256e8669cb18e8dca80c791ed3
Peggy <peggy.hoelzel@outlook.de>
2019-02-28 11:16:50 +0100

    added LineFollower class with sensor variables

--- src/odometry.py
+++ src/odometry.py
@@ -2,3 +2,36 @@
 
 #  Suggestion: 	implement odometry as class that is not using the ev3dev.ev3 package
 # 				establish value exchange with main driving class via getters and setters
+
+import ev3dev.ev3 as ev3
+
+class LineFollower():
+    def __init__(self):
+        stop = False
+
+    def drive(self):
+        stop = False
+
+        #sensors
+        colorSensor = ev3.ColorSensor()
+        ultrasonicSensor = ev3.UltrasonicSensor()
+
+        assert colorSensor.connected
+        assert ultrasonicSensor.connected
+
+        #motors
+        rightMotor = ev3.LargeMotor(ev3.OUTPUT_C)
+        leftMotor = ev3.LargeMotor(ev3.OUTPUT_B)
+
+        assert rightMotor.connected
+        assert leftMotor.connected
+
+        while(not stop):
+            #drive
+
+
+class Odometry():
+    #code
+
+if __name__ == "__main__":
+    #run LineFollower
