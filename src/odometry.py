733fe7615e77b14a6a8e61c5cc15edde6568d4c1
Peggy <peggy.hoelzel@outlook.de>
2019-03-03 23:04:18 +0100

    added position function to odometry

--- src/odometry.py
+++ src/odometry.py
@@ -6,8 +6,22 @@ import math
 
 class Odometry():
     r = 2.5         # Radius Räder
+    a = 0           # Radabstand
     dx = 0          # Streckendifferenz
     dy = 0
     dl = 0          # zurückgelegte Strecke des linken Rads
     dr = 0
+    s = 0           # zurückgelegte Strecke
+    #gamma = Blickrichtung
 
+    def position(self, gamma):
+        alpha = (self.dl + self.dr)/self.a
+        s = (self.dr + self.dl)/alpha * math.sin(alpha/2)
+
+        dx = - math.sin(gamma + (alpha/2)) * s
+        dy = math.cos(gamma + (alpha/2)) * s
+
+        gamma += alpha
+
+        # take list of [gamma, dl, dr]s and start position
+        # return calculated position
