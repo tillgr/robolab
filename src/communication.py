45a3bc7925d034d5d1035f25dbb10976bb930c6b
unknown <vasil petro>
2019-03-05 14:58:13 +0100

    com

--- src/communication.py
+++ src/communication.py
@@ -10,12 +10,16 @@ class Communication:
     def on_connect(self):
         #client = mqtt.Client(client_id="039", clean_session=False, protocol=mqtt.MQTTv31)
         #client.on_message = on_message # Assign pre-defined callback function to MQTT client
-        self.client.username_pw_set('039', password='vasil241') # Your group credentials
+        self.client.username_pw_set('039', password='aEraXzm9Xq') # Your group credentials
         self.client.connect('mothership.inf.tu-dresden.de', port=8883)
         self.client.subscribe('explorer/039', qos=1) # Subscribe to topic explorer/xxx
-        client.loop_start()
+        self.client.loop_start()
+        self.send_message("","")
         while True:
-	           input('Press Enter to continue...\n')
+            self.send_message1("","")
+            input('Press Enter to continue...\n')
+
+
 
     def __init__(self, mqtt_client):
         """ Initializes communication module, connect to server, subscribe, etc. """
@@ -27,16 +31,31 @@ class Communication:
 
         # ADD YOUR VARIABLES HERE
 
-        self.planet_string= '''
-            {
-                "from":"client",
-                "type":"testplanet",
-                "payload":
-                    {
-                        "planetName":"Kashyyyk"
-                    }
-            }
-        '''
+        planetName="Kashyyyk" #receives planet name
+        type=""  #receives path or planet
+        #Coordinates
+        startX=""  #punkt = f"x:{startX}"
+        startY=""
+        start_punkt=('startX','startY','startDirection')
+        endY=""
+        endX=""
+        end_punkt=('endX','endY')
+        targetX=""
+        targetY=""
+        target_punkt=('targetX','targetY')
+        #Directions in degrees
+        #NORTH = 0
+        #EAST = 90
+        #SOUTH = 180
+        #WEST = 270
+        startDirection=""
+        endDirection=""
+
+        pathStatus="" #receives blocked|free
+        pathweight="" #when blocked (-1) ,when free (>0)
+
+
+
     # THIS FUNCTIONS SIGNATURE MUST NOT BE CHANGED
     def on_message(self, client, data, message):
         """ Handles the callback if any message arrived """
@@ -47,12 +66,19 @@ class Communication:
 
         pass
 
+    def send_message1(self, topic, message):
+        """ Sends given message to specified channel """
+        #msg = json.loads(self.planet_string)
+        self.client.publish("Kashyyyk-039",'{"from": "client","type": "path","payload": {"startX": "<Xs>","startY": "<Ys>","startDirection": "<Ds>","endX": "<Xe>","endY": "<Ye>","endDirection": "<De>","pathStatus": "free|blocked" }}')
+        #'{"from":"client","type":"testplanet","payload":{planetName}')
+        pass
+
     def send_message(self, topic, message):
         """ Sends given message to specified channel """
         #msg = json.loads(self.planet_string)
-        self.client.publish("explorer/039", payload=self.planet_string, qos=1)
+        self.client.publish("explorer/039",'{"from": "client","type": "ready"}')
         pass
 
     def quit_connection(self):
-        client.loop_stop()
-        client.disconnect()
+        self.client.loop_stop()
+        self.client.disconnect()
