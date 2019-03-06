d90bff8b5ebab2b2a2bef27a6e0be5d26f6e7c20
unknown <vasil petro>
2019-03-06 11:21:48 +0100

    newcom

--- src/communication.py
+++ src/communication.py
@@ -7,9 +7,35 @@ class Communication:
         Class to hold the MQTT client
         Feel free to add functions, change the constructor and the example send_message() to satisfy your requirements and thereby solve the task according to the specifications
     """
+
+        # ADD YOUR VARIABLES HERE
+
+    planetName="" #receives planet name
+    type=""  #receives path or planet
+        #Coordinates
+    startX=""  #punkt = f"x:{startX}"
+    startY=""
+    start_punkt=('startX','startY','startDirection')
+    endY=""
+    endX=""
+    end_punkt=('endX','endY')
+    targetX=""
+    targetY=""
+    target_punkt=('targetX','targetY')
+        #Directions in degrees
+        #NORTH = 0
+        #EAST = 90
+        #SOUTH = 180
+        #WEST = 270
+    startDirection=""
+    endDirection=""
+
+    pathStatus="" #receives blocked|free
+    pathweight="" #when blocked (-1) ,when free (>0)
+
     def on_connect(self):
-        #client = mqtt.Client(client_id="039", clean_session=False, protocol=mqtt.MQTTv31)
-        #client.on_message = on_message # Assign pre-defined callback function to MQTT client
+            #client = mqtt.Client(client_id="039", clean_session=False, protocol=mqtt.MQTTv31)
+            #client.on_message = on_message # Assign pre-defined callback function to MQTT client
         self.client.username_pw_set('039', password='aEraXzm9Xq') # Your group credentials
         self.client.connect('mothership.inf.tu-dresden.de', port=8883)
         self.client.subscribe('explorer/039', qos=1) # Subscribe to topic explorer/xxx
@@ -19,42 +45,13 @@ class Communication:
             self.send_message1("","")
             input('Press Enter to continue...\n')
 
-
-
     def __init__(self, mqtt_client):
-        """ Initializes communication module, connect to server, subscribe, etc. """
-        # THESE TWO VARIABLES MUST NOT BE CHANGED
+            #""" Initializes communication module, connect to server, subscribe, etc. """
+            # THESE TWO VARIABLES MUST NOT BE CHANGED
         self.client = mqtt_client
         self.client.on_message = self.on_message
         self.on_connect()
         self.quit_connection()
-
-        # ADD YOUR VARIABLES HERE
-
-        planetName="Kashyyyk" #receives planet name
-        type=""  #receives path or planet
-        #Coordinates
-        startX=""  #punkt = f"x:{startX}"
-        startY=""
-        start_punkt=('startX','startY','startDirection')
-        endY=""
-        endX=""
-        end_punkt=('endX','endY')
-        targetX=""
-        targetY=""
-        target_punkt=('targetX','targetY')
-        #Directions in degrees
-        #NORTH = 0
-        #EAST = 90
-        #SOUTH = 180
-        #WEST = 270
-        startDirection=""
-        endDirection=""
-
-        pathStatus="" #receives blocked|free
-        pathweight="" #when blocked (-1) ,when free (>0)
-
-
 
     # THIS FUNCTIONS SIGNATURE MUST NOT BE CHANGED
     def on_message(self, client, data, message):
@@ -76,7 +73,7 @@ class Communication:
     def send_message(self, topic, message):
         """ Sends given message to specified channel """
         #msg = json.loads(self.planet_string)
-        self.client.publish("explorer/039",'{"from": "client","type": "ready"}')
+        self.client.publish("explorer/039",'{"form":"client","type":"ready"}')
         pass
 
     def quit_connection(self):
